54b823c547cb36015feeb3dfbee7e45c0e72c501
jsh <jsh@a.com>
2021-11-26 12:50:52 +0900

    Add assertj dependency

---
+++ src/test/java/AssertjTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AssertjTest {
+  @Test
+  void testLibraryInstall() {
+    assertThat(true).isTrue();
+  }
+}
+
