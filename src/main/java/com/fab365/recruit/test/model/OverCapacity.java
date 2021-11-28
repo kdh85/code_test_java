8fb40a269fbb8a7103f9653c6e40130f1c30561d
kdh85 <82087284+kdh85@users.noreply.github.com>
2021-11-28 13:57:23 +0900

    feat: 초과 사용량 기능 구현 * 초과사용량과 요금제를 주입받아서 초과된 사용량 객체를 반환. * 초과사용량이 없는 경우 초과사용량을 0으로 반환. * 초과사용량 입력값이 0 미만인경우 에러를 반환.

--- src/main/java/com/fab365/recruit/test/model/OverCapacity.java
+++ src/main/java/com/fab365/recruit/test/model/OverCapacity.java
@@ -2,6 +2,10 @@ package com.fab365.recruit.test.model;
 
 public class OverCapacity extends Payment {
 
+	private static final int MIN_OVER_CAPACITY_VALUE = 0;
+	private static final String ERROR_MIN_OVER_CAPACITY_VALUE = String.format("초과 사용량은 %d 이상부터 가능합니다.",
+		MIN_OVER_CAPACITY_VALUE);
+
 	private int overCapacity;
 
 	public OverCapacity() {
@@ -13,6 +17,20 @@ public class OverCapacity extends Payment {
 
 	@Override
 	public OverCapacity overCapacityCalculate(int usageDataInMegabyte, Fee fee) {
+
+		validationUseData(usageDataInMegabyte);
+
+		this.overCapacity = usageDataInMegabyte - fee.getBasicFreeCapacity();
+
+		if (this.overCapacity < MIN_OVER_CAPACITY_VALUE) {
+			this.overCapacity = MIN_OVER_CAPACITY_VALUE;
+		}
 		return this;
+	}
+
+	private void validationUseData(int usageDataInMegabyte) {
+		if (usageDataInMegabyte < MIN_OVER_CAPACITY_VALUE) {
+			throw new IllegalArgumentException(ERROR_MIN_OVER_CAPACITY_VALUE);
+		}
 	}
 }
