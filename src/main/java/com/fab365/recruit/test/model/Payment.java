114c4590cffcfb85cc3cc6639b137df35cd82474
kdh85 <82087284+kdh85@users.noreply.github.com>
2021-11-29 00:13:38 +0900

    refactor: 메소드에 불필요한 파라미터 & 객체 반환을 제거 및 메세지 상수화 * 코드개선과정에서 불필요하게 객체를 반환하거나 주입하는 파라메터를 제거함. * 에러메세지들을 상수 처리함.

--- src/main/java/com/fab365/recruit/test/model/Payment.java
+++ src/main/java/com/fab365/recruit/test/model/Payment.java
@@ -4,19 +4,23 @@ import com.fab365.recruit.test.PricingPlan;
 
 public abstract class Payment implements PricingPlan {
 
+	private static final String ERROR_TOTAL_CALCULATE = "총 사용량을 계산을 할 수 없습니다.";
+	private static final String ERROR_OVER_CAPACITY = "초과 사용량 계산을 할 수 없습니다.";
+	private static final String ERROR_OVER_AMOUNT = "초과 요금 계산을 할 수 없습니다.";
+
 	@Override
-	public TotalUserAmount totalAmountCalculate(int overAmount, Fee fee) {
-		throw new IllegalArgumentException("총 사용량을 계산을 할 수 없습니다.");
+	public TotalUserAmount totalAmountCalculate(Fee fee) {
+		throw new IllegalArgumentException(ERROR_TOTAL_CALCULATE);
 	}
 
 	@Override
-	public OverCapacity overCapacityCalculate(int usageDataInMegabyte, Fee fee) {
-		throw new IllegalArgumentException("초과 사용량 계산을 할 수 없습니다.");
+	public void overCapacityCalculate(int usageDataInMegabyte, Fee fee) {
+		throw new IllegalArgumentException(ERROR_OVER_CAPACITY);
 	}
 
 	@Override
-	public OverAmount overAmountCalculate(int overCapacity, BasicOverCost basicOverCost) {
-		throw new IllegalArgumentException("초과 요금 계산을 할 수 없습니다.");
+	public OverAmount overAmountCalculate(BasicOverCost basicOverCost) {
+		throw new IllegalArgumentException(ERROR_OVER_AMOUNT);
 	}
 
 }
