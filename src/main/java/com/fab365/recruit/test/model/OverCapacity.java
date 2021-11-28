ee6d22a03b7a5a7543ca9f4bba2c929567897ada
kdh85 <82087284+kdh85@users.noreply.github.com>
2021-11-28 15:45:33 +0900

    feat: 초과용량 객체비교 추가. * 객체 생성시 equals 비교가 가능하도록 equals 및 hashCode override 구현.

--- src/main/java/com/fab365/recruit/test/model/OverCapacity.java
+++ src/main/java/com/fab365/recruit/test/model/OverCapacity.java
@@ -1,5 +1,7 @@
 package com.fab365.recruit.test.model;
 
+import java.util.Objects;
+
 public class OverCapacity extends Payment {
 
 	public static final int MIN_OVER_CAPACITY_VALUE = 0;
@@ -32,5 +34,27 @@ public class OverCapacity extends Payment {
 		if (usageDataInMegabyte < MIN_OVER_CAPACITY_VALUE) {
 			throw new IllegalArgumentException(ERROR_MIN_OVER_CAPACITY_VALUE);
 		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		OverCapacity that = (OverCapacity)o;
+		return overCapacity == that.overCapacity;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(overCapacity);
+	}
+
+	@Override
+	public String toString() {
+		return "OverCapacity{" +
+			   "overCapacity=" + overCapacity +
+			   '}';
 	}
 }
